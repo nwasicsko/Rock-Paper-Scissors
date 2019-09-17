4572baf562edecd92f8214383bccd44396711145
nwasicsko <33705937+nwasicsko@users.noreply.github.com>
2019-09-17 19:12:48 -0400

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,25 @@
+import random
+
+choice = ["rock", "paper","scissors"]
+
+computer = choice[random.randint(0,2)]
+
+print('Welcome to the Rock, Paper, Scissors Game\n')
+player = input("Your Choice: ").lower()
+
+print("Computer Choice: " + computer)
+
+if player == computer:
+    print("Draw")
+elif player == "rock" and computer == "paper":
+    print("Computer Wins")
+elif player == "rock" and computer == "scissors":
+    print("Player Wins")
+elif player == "paper" and computer == "rock":
+    print("Player Wins")
+elif player == "paper" and computer == "scissors":
+    print("Computer Wins")
+elif player == "scissors" and computer == "rock":
+    print("Computer Wins")
+elif player == "scissors" and computer == "paper":
+    print("Player Wins")
